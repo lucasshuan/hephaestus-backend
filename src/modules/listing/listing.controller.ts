42af9f5650268d1f7a86cef08bf69217b94ba740
lucasshuan <jlsrocha1@gmail.com>
2025-10-22 16:45:03 -0300

    feat: list listings

--- src/modules/listing/listing.controller.ts
+++ src/modules/listing/listing.controller.ts
@@ -1,12 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ListingService } from './listing.service';
-import { ApiCookieAuth } from '@nestjs/swagger';
+import { ApiCookieAuth, ApiOperation } from '@nestjs/swagger';
 
 @ApiCookieAuth('session')
 @Controller('listings')
 export class ListingController {
   constructor(private readonly listingService: ListingService) {}
 
+  @ApiOperation({
+    summary: 'List listings',
+    description: 'Get all listings.',
+  })
+  @Get()
   async findAll() {
     return this.listingService.findAll();
   }
